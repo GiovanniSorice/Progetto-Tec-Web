47112c52bc7db9ea18dfae429488b45e8fecdfae
GiovanniSorice <giovanni.sorice.10@gmail.com>
2019-02-02 19:20:39 +0100

    Aggiornata search bar

--- public/php/livesearch.php
+++ public/php/livesearch.php
@@ -6,30 +6,52 @@ include_once 'general_function.php';
 $q=$_GET["q"];
 
 //lookup all links from the xml file if length of q>0
+$hint="";
 
-if (strlen($q)>0) {
-    $hint="";       
+if (strlen($q)>0) {    
     
-    $query="select id, titolo from serie where titolo like concat('%',?,'%')";
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $extra = 'serie.php?serie_id=';
+    
+    $query="select id,miniatura,titolo, consigliato from serie where titolo like concat('%',?,'%')";
     //Seleziono la lista dei generi già in una tabella
     $stmt=executeQuery($query,array(&$q),array("s"));
     $series=resultQueryToTable($stmt->get_result());
     $stmt->close();
+    
+    $genere_page = implode("",file("../txt/genere.txt"));
+    $show_page = implode("",file("../txt/show.txt"));
+    
+    $genere_show_collect="<!-- Successivo -->";
+            
+    //Scrivo le serie tv
+    $show="<!-- Successiva -->";
+    
+    /*if (count($series)>1) {
+     $show=$show_page;
+     }*/
+    
     foreach ($series as $serie) {
-        //find a link matching the search text
-            if ($hint=="") {
-                $hint="<a href='" .
-                    $serie["id"] .
-                    "' target='_blank'>" .
-                    $serie["titolo"]. "</a>";
-            } else {
-                $hint=$hint . "<br /><a href='" .
-                    $serie["id"] .
-                    "' target='_blank'>" .
-                    $serie["titolo"] . "</a>";
-            }
+        $hint="1";
+        $show=preg_replace("/<!-- Successiva -->/i",$show_page." <!-- Successiva -->" , $show );
+        $show=preg_replace("/<!-- Immagine -->/i",$serie["miniatura"] , $show );
+        $show=preg_replace("/<!-- Id -->/i",$serie["id"] , $show );
+        $show=preg_replace("/<!-- Titolo -->/i",$serie["titolo"] , $show );
+        $show=preg_replace("/<!-- Consigliato -->/i",$serie["consigliato"] , $show );
+        $show=preg_replace("/<!-- Url_Show -->/i","http://$host$uri/$extra".$serie["id"] , $show );
     }
+    
+    $show=preg_replace("/<!-- Successiva -->/i","" , $show );
+    
+    //Inserisco le serie nel div del genere e completo il div con le informazioni mancanti
+    $genere_show_collect=preg_replace("/<!-- Successivo -->/i",$genere_page , $genere_show_collect );
+    $genere_show_collect=preg_replace("/<!-- Genere -->/i","Risultati ricerca" , $genere_show_collect );
+    $genere_show_collect=preg_replace("/<!-- Genere_Titolo -->/i","Risultati ricerca" , $genere_show_collect );
+    $genere_show_collect=preg_replace("/<!-- Show -->/i",$show , $genere_show_collect );
+    
 }
+    
 
 
 // Set output to "no suggestion" if no hint was found
@@ -37,7 +59,7 @@ if (strlen($q)>0) {
 if ($hint=="") {
   $response="no suggestion";
 } else {
-  $response=$hint;
+    $response=$genere_show_collect;
 }
 
 //output the response
