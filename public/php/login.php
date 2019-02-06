54ddd35f1e470386d67ef972867e9fac54945db0
GiovanniSorice <giovanni.sorice.10@gmail.com>
2019-02-06 20:54:48 +0100

    Fix session_start()

--- public/php/login.php
+++ public/php/login.php
@@ -1,8 +1,6 @@
 <?php 
     include_once 'template.php';
     
-    session_start();
-    
     if(array_key_exists('user_id',$_SESSION) && !empty($_SESSION['user_id'])){
         createPage("esplora");
     }else{
