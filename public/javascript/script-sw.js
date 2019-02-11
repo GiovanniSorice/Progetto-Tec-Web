83aeaf40f7f00a99619cec799adafcfd320b5d9e
Lorenzo Busin <lorenzo.busin@gmail.com>
2019-02-11 12:07:06 +0100

    fix SW

--- public/javascript/script-sw.js
+++ public/javascript/script-sw.js
@@ -1,9 +1,8 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('../service-worker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+ // Path che contiene il service worker
+ navigator.serviceWorker.register('../service-worker.js').then(function(registration) {
+   console.log('Service worker installato correttamente, ecco lo scope:', registration.scope);
+ }).catch(function(error) {
+   console.log('Installazione service worker fallita:', error);
+ });
 }
